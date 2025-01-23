6132d4acd49559743c8d7d6af31910c3dd51a457
Mads Lee Giil <mads@giil.no>
2025-01-23 10:17:55 +0900

    Add more test cases

--- src/test/kotlin/com/madslee/nullcheck/NullCheckTest.kt
+++ src/test/kotlin/com/madslee/nullcheck/NullCheckTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.shouldBe
 import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
 import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import javax.sql.DataSource
@@ -24,18 +25,44 @@ class NullCheckTest {
         NullCheck.setUp(dataSource)
     }
 
+    @BeforeEach
+    fun beforeEach() {
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("truncate table nullcheck_field, nullcheck_class").execute()
+        }
+    }
+
     @Test
-    fun `Instantiating Person-object with null for age shall be counted in the database`() {
+    fun `Instantiating an object shall be counted`() {
+        Person(null)
+        Person(1)
         Person(null)
 
         val nullCheckClass = getNullCheckClasses(dataSource)
         nullCheckClass shouldHaveSize 1
         nullCheckClass[0].className shouldBe "Person"
-        nullCheckClass[0].numberOfInstantiations shouldBe 1
+        nullCheckClass[0].numberOfInstantiations shouldBe 3
+    }
 
+    @Test
+    fun `Field with null shall be counted correctly`() {
+        Person(null)
+
+        val nullCheckClass = getNullCheckClasses(dataSource)
         val classFields = nullCheckClass.first().nullCheckFields
         classFields shouldHaveSize 1
         classFields[0].fieldName shouldBe "age"
         classFields[0].numberOfTimesNull shouldBe 1
+    }
+
+    @Test
+    fun `Field with not null shall be counted correctly`() {
+        Person(1)
+
+        val nullCheckClass = getNullCheckClasses(dataSource)
+        val classFields = nullCheckClass.first().nullCheckFields
+        classFields shouldHaveSize 1
+        classFields[0].fieldName shouldBe "age"
+        classFields[0].numberOfTimesNull shouldBe 0
     }
 }
